a65b9b7cd62aa41cb5c0ed580c9b24e23c8bfa83
A Praneeth <76730173+praneeth-arasada@users.noreply.github.com>
2021-06-10 13:42:08 +0530

    Update and rename blogs-download.py to blogs.py

--- blogs.py
+++ blogs.py
@@ -5,9 +5,27 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
 from urllib.request import urlopen
 from xml.etree.ElementTree import parse
-   
+from celery import Celery
+from celery.schedules import crontab
+     
+app = Celery('blogs')
+
+app.conf.timezone = 'UTC'
+
+app.conf.beat_schedule = {
+    # executes every 1 minute
+    'scraping-task-one-min': {
+        'task': 'blogs.download_blogs', 
+        'schedule': crontab(),
+    #     'schedule': crontab(minute=0, hour=0)
+    #     'schedule': crontab(minute='*/15')
+    },
+}
+ 
   
 def download_blog_from_xml(dir, url, xmldoc, utf):
+    if not os.path.exists(dir):
+    		os.makedirs(dir)    
     try:
         output_file = dir
         files = os.listdir(dir)
@@ -100,87 +118,91 @@ def download_blog_from_xml(dir, url, xmldoc, utf):
     except Exception as e:
         print('Error. See exception:')
         print(e) 
-    
         
-def function():
-        article ={1:{
-                        'title': 'Starting: ThreatPost' ,
-                        'dir': r'C:\Users\alpra\Documents\blog-down\ThreatPost' ,
-                        'link': 'https://threatpost.com/feed/',
-                        'xmldoc': False,
-                        'utf': False,
-                        'update': 'Updated: ThreatPost'
-                        }, 
-                  2: {
-                        'title': 'Starting: Zdnet' ,
-                        'dir': r'C:\Users\alpra\Documents\blog-down\Zero Day' ,
-                        'link': 'https://www.zdnet.com/blog/rss.xml',
-                        'xmldoc': True,
-                        'utf': False,
-                        'update': 'Updated: Zdnet'
-                        },
-                  3: {
-                        'title': 'Starting: NakedSecurity' ,
-                        'dir': r'C:\Users\alpra\Documents\blog-down\Naked Security' ,
-                        'link': 'https://nakedsecurity.sophos.com/feed/',
-                        'xmldoc': True,
-                        'utf': False,
-                        'update': 'Updated: NakedSecurity'
-                        },
-                  4: {
-                        'title': 'Starting: DarkReading' ,
-                        'dir': r'C:\Users\alpra\Documents\blog-down\Dark Reading' ,
-                        'link': 'https://www.darkreading.com/rss_simple.asp?f_n=659&f_ln=Threat%20Intelligence',
-                        'xmldoc': False,
-                        'utf': True,
-                        'update': 'Updated: DarkReading'
-                        },
-                  5: {
-                        'title': 'Starting: GrahamCluley' ,
-                        'dir': r'C:\Users\alpra\Documents\blog-down\GrahamCluley' ,
-                        'link': 'https://grahamcluley.com/feed/',
-                        'xmldoc': False,
-                        'utf': True,
-                        'update': 'Updated: GrahamCluley'
-                        },
-                  6: {
-                        'title': 'Starting: TechNewsWorld' ,
-                        'dir': r'C:\Users\alpra\Documents\blog-down\Tech News World' ,
-                        'link': 'https://www.technewsworld.com/perl/syndication/rssfull.pl?__hstc=67659214.41033b4a8973e2312a56265f9f08be64.1623167946568.1623167946568.1623167946568.1&__hssc=67659214.2.1623167946570&__hsfp=4264810217',
-                        'xmldoc': True,
-                        'utf': False,
-                        'update': 'Updated: TechNewsWorld'
-                        },
-                  7: {
-                        'title': 'Starting: The Hacker News' ,
-                        'dir': r'C:\Users\alpra\Documents\blog-down\The Hacker News' ,
-                        'link': 'https://feeds.feedburner.com/TheHackersNews',
-                        'xmldoc': True,
-                        'utf': False,
-                        'update': 'Updated: The Hacker News'
-                        },
-                  8: {
-                        'title': 'Starting: Information Security Buzz' ,
-                        'dir': r'C:\Users\alpra\Documents\blog-down\Information Security Buzz' ,
-                        'link': 'https://feeds.feedburner.com/InformationSecurityBuzz',
-                        'xmldoc': True,
-                        'utf': False,
-                        'update': 'Updated: Information Security Buzz'
-                        },
-                  9: {
-                        'title': 'Starting: Krebs on Security' ,
-                        'dir': r'C:\Users\alpra\Documents\blog-down\Krebs on Security' ,
-                        'link': 'https://krebsonsecurity.com/feed/',
-                        'xmldoc': False,
-                        'utf': True,
-                        'update': 'Updated: Krebs on Security'
-                        }
-                 }
+          
+@app.blogs         
+def download_blogs():
 
-        for i in range(1,10):
-            print(article[i]['title']) 
-            print() 
-            download_blog_from_xml(article[i]['dir'], article[i]['link'], article[i]['xmldoc'], article[i]['utf'])
-            print(article[i]['update'])
-            print()
-function()
+	article ={1:{
+		        'title': 'Starting: ThreatPost' ,
+		        'dir': r'/home/praneeth/Documents/blog-down/ThreatPost' ,
+		        'link': 'https://threatpost.com/feed/',
+		        'xmldoc': False,
+		        'utf': False,
+		        'update': 'Updated: ThreatPost'
+		        }, 
+		  2: {
+		        'title': 'Starting: Zdnet' ,
+		        'dir': r'/home/praneeth/Documents/blog-down/Zero Day' ,
+		        'link': 'https://www.zdnet.com/blog/rss.xml',
+		        'xmldoc': True,
+		        'utf': False,
+		        'update': 'Updated: Zdnet'
+		        },
+		  3: {
+		        'title': 'Starting: NakedSecurity' ,
+		        'dir': r'/home/praneeth/Documents/blog-down/Naked Security' ,
+		        'link': 'https://nakedsecurity.sophos.com/feed/',
+		        'xmldoc': True,
+		        'utf': False,
+		        'update': 'Updated: NakedSecurity'
+		        },
+		  4: {
+		        'title': 'Starting: DarkReading' ,
+		        'dir': r'/home/praneeth/Documents/blog-down/Dark Reading' ,
+		        'link': 'https://www.darkreading.com/rss_simple.asp?f_n=659&f_ln=Threat%20Intelligence',
+		        'xmldoc': False,
+		        'utf': True,
+		        'update': 'Updated: DarkReading'
+		        },
+		  5: {
+		        'title': 'Starting: GrahamCluley' ,
+		        'dir': r'/home/praneeth/Documents/blog-down/GrahamCluley' ,
+		        'link': 'https://grahamcluley.com/feed/',
+		        'xmldoc': False,
+		        'utf': True,
+		        'update': 'Updated: GrahamCluley'
+		        },
+		  6: {
+		        'title': 'Starting: TechNewsWorld' ,
+		        'dir': r'/home/praneeth/Documents/blog-down/Tech News World' ,
+		        'link': 'https://www.technewsworld.com/perl/syndication/rssfull.pl?__hstc=67659214.41033b4a8973e2312a56265f9f08be64.1623167946568.1623167946568.1623167946568.1&__hssc=67659214.2.1623167946570&__hsfp=4264810217',
+		        'xmldoc': True,
+		        'utf': False,
+		        'update': 'Updated: TechNewsWorld'
+		        },
+		  7: {
+		        'title': 'Starting: The Hacker News' ,
+		        'dir': r'/home/praneeth/Documents/blog-down/The Hacker News' ,
+		        'link': 'https://feeds.feedburner.com/TheHackersNews',
+		        'xmldoc': True,
+		        'utf': False,
+		        'update': 'Updated: The Hacker News'
+		        },
+		  8: {
+		        'title': 'Starting: Information Security Buzz' ,
+		        'dir': r'/home/praneeth/Documents/blog-down/Information Security Buzz' ,
+		        'link': 'https://feeds.feedburner.com/InformationSecurityBuzz',
+		        'xmldoc': True,
+		        'utf': False,
+		        'update': 'Updated: Information Security Buzz'
+		        },
+		  9: {
+		        'title': 'Starting: Krebs on Security' ,
+		        'dir': r'/home/praneeth/Documents/blog-down/Krebs on Security' ,
+		        'link': 'https://krebsonsecurity.com/feed/',
+		        'xmldoc': False,
+		        'utf': True,
+		        'update': 'Updated: Krebs on Security'
+		        }
+		 }   
+		 
+	for i in range(1,10):
+		print(article[i]['title']) 
+		print()
+		download_blog_from_xml(article[i]['dir'], article[i]['link'], article[i]['xmldoc'], article[i]['utf'])
+		print(article[i]['update'])
+		print()
+		 
+          
+download_blogs()
